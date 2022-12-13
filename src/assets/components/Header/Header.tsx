69fe3c89b6e8ec900fcf65529710a39537502b16
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2022-12-13 16:59:42 +0300

    create Header

---
+++ src/assets/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+import styles from './Header.module.scss';
+import { HeaderProps } from './Header.props';
+
+const Header: FC<HeaderProps> = () => {
+  return <header></header>;
+};
+
+export default Header;
--- src/assets/components/Layout/Layout.tsx
+++ src/assets/components/Layout/Layout.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from 'react';
 import { LayoutProps } from './Layout.props';
+import Header from '../Header/Header';
 
 const Layout: FC<LayoutProps> = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
 };
 
 export default Layout;
