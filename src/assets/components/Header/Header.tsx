f338674081a2a3e96829d85f523061ea33ddfb5b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2022-12-13 23:27:17 +0300

    update Header
    Added logo to header

--- src/assets/components/Header/Header.tsx
+++ src/assets/components/Header/Header.tsx
@@ -1,9 +1,19 @@
 import React, { FC } from 'react';
 import styles from './Header.module.scss';
 import { HeaderProps } from './Header.props';
+import cn from 'classnames';
+import { Link } from 'react-router-dom';
 
 const Header: FC<HeaderProps> = () => {
-  return <header></header>;
+  return (
+    <header className={cn(styles.appHeader)}>
+      <div className={cn(styles.container)}>
+        <Link to={'/'} className={cn(styles.logo, styles.cell)}>
+          ЯСК
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
