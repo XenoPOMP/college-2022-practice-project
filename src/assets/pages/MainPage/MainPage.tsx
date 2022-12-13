38effc98afa600b8e8aa9faf81f1fb26c0c717e0
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2022-12-13 16:54:59 +0300

    import all fonts

--- src/assets/pages/MainPage/MainPage.tsx
+++ src/assets/pages/MainPage/MainPage.tsx
@@ -3,9 +3,11 @@ import Page from '../../components/Page/Page';
 
 const MainPage = () => {
   return (
-    <Page
-      meta={{ pageTitle: 'Main', pageDescription: '', keywords: '' }}
-    ></Page>
+    <Page meta={{ pageTitle: 'Main', pageDescription: '', keywords: '' }}>
+      <h1>Это шапка</h1>
+      <h2>Это заголовок внутри контента</h2>
+      <span>Это обычный текст</span>
+    </Page>
   );
 };
 
