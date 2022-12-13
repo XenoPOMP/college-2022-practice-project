5feb60a9ceb8330f4914b6d8b79e1aa1d20e2e38
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2022-12-13 16:37:20 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './assets/pages/MainPage/MainPage';
+import NotFound from './assets/pages/NotFound/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path={'*'} element={<NotFound />} />
+
+        <Route path={'/'} element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
